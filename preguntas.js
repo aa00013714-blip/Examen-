9e3d609455c68a666c18e4236e77bc3af9b0a276
aa00013714-blip <aa00013714@my.uvm.edu.mx>
2026-05-14 10:11:20 -0600

    Create preguntas.js

---
+++ preguntas.js
@@ -0,0 +1,91 @@
+const preguntas = [
+
+{
+pregunta: "¿Qué es un gen?",
+opciones: [
+"Un tipo de célula",
+"Fragmento de ADN con información hereditaria",
+"Un cromosoma completo",
+"Una proteína"
+],
+correcta: 1
+},
+
+{
+pregunta: "¿Qué es el locus?",
+opciones: [
+"La unión de cromosomas",
+"La característica observable",
+"Lugar específico que ocupa un gen en un cromosoma",
+"Un alelo dominante"
+],
+correcta: 2
+},
+
+{
+pregunta: "Los alelos son:",
+opciones: [
+"Tipos de células",
+"Formas alternativas de un mismo gen",
+"Cromosomas sexuales",
+"Mutaciones siempre dañinas"
+],
+correcta: 1
+},
+
+{
+pregunta: "La dominancia ocurre cuando:",
+opciones: [
+"Dos genes desaparecen",
+"Un alelo se expresa sobre otro",
+"El gen es recesivo",
+"No existe herencia"
+],
+correcta: 1
+},
+
+{
+pregunta: "¿Qué es una mutación?",
+opciones: [
+"Un órgano celular",
+"Un cambio en el material genético",
+"Una proteína",
+"Un aminoácido"
+],
+correcta: 1
+},
+
+{
+pregunta: "¿Qué ocurre en una mutación por inserción?",
+opciones: [
+"Se agrega un nucleótido",
+"Se elimina un nucleótido",
+"Se divide la célula",
+"Se forman proteínas"
+],
+correcta: 0
+},
+
+{
+pregunta: "El ARN se diferencia del ADN porque:",
+opciones: [
+"Tiene timina",
+"Tiene uracilo",
+"Tiene doble cadena siempre",
+"No tiene bases nitrogenadas"
+],
+correcta: 1
+},
+
+{
+pregunta: "¿Qué propone la teoría de Darwin-Wallace?",
+opciones: [
+"La evolución por selección natural",
+"El uso y desuso",
+"El fijismo",
+"La generación espontánea"
+],
+correcta: 0
+}
+
+];
